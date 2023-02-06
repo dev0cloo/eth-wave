780773c2c83695065477b8441a00bfb04ddfc7b3
dev0cloo <etor.ocloo@gmail.com>
2023-02-06 02:05:26 +0000

    Update WavePortal.sol

--- smart-contracts/contracts/WavePortal.sol
+++ smart-contracts/contracts/WavePortal.sol
@@ -5,19 +5,30 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    // create new event anytime a message is sent with the senders details
+    event newMessage(address indexed from, string, uint256 timestamp);
+    // create a struct for storing messagers details
+    struct Messages {
+        address messenger;
+        string textMessage;
+        uint256 timestamp;
+    }
+    // create new empty array from Messages struct
+    Messages[] message;
 
-    uint256 totalWaves;
-
-    constructor() { console.log('Smart contract is being constructed.');         
+    constructor() {
+        console.log("Smart contract is being constructed.");
     }
 
-    function wave () public {
-        totalWaves += 1 ;
-        console.log('%s has waved', msg.sender);
+    function sendMessage(string memory _message) public {
+        console.log("%s has waved", msg.sender, _message);
+        // log the message to messages array
+        message.push(Messages(msg.sender, _message, block.timestamp));
+
+        emit newMessage(msg.sender, _message, block.timestamp);
     }
 
-    function getWaves () public view returns (uint256){
-        console.log ('We have %d waves', totalWaves);
-        return totalWaves;
+    function getMessages() public view returns (Messages[] memory) {
+        return message;
     }
 }
