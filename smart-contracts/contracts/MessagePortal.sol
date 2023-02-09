bc53b4d0c9584c0f719ca324f7e0792c5ba8696e
dev0cloo <etor.ocloo@gmail.com>
2023-02-09 17:27:19 +0000

    Update smart contract

--- smart-contracts/contracts/MessagePortal.sol
+++ smart-contracts/contracts/MessagePortal.sol
@@ -27,6 +27,14 @@ contract MessagePortal {
         message.push(Messages(msg.sender, _message, block.timestamp));
 
         emit newMessage(msg.sender, _message, block.timestamp);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Not enough funds in the contract to make this transaction"
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw amount from contract");
     }
 
     // return all messages in contract storage
